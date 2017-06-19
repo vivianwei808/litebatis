e23e675fa695f77ad85c7d7ff71c50c1061c0e05
liuchengb@zbj.com <liuchengb@zbj.com>
2017-06-19 20:09:18 +0800

    修改

--- litebatis-api/src/main/java/org/wing4j/litebatis/reflection/MetaObject.java
+++ litebatis-api/src/main/java/org/wing4j/litebatis/reflection/MetaObject.java
@@ -1,28 +1,65 @@
-//package org.wing4j.litebatis.reflection;
-//
-//import org.wing4j.litebatis.reflection.factory.ObjectFactory;
-//import org.wing4j.litebatis.reflection.wrapper.ObjectWrapperFactory;
-//
-//import java.util.List;
-//
-///**
-// * Created by wing4j on 2017/5/16.
-// * 用于对实体类进行包装，提供字段、方法的元信息
-// */
-//public abstract class MetaObject {
-//    public abstract boolean hasGetter(String name);
-//
-//    public abstract void setValue(String name, Object value);
-//
-//    public abstract Object getValue(String name);
-//    public abstract Class<?> getSetterType(String name);
-//    public abstract boolean hasSetter(String name);
-//    public abstract Class<?> getGetterType(String name);
-//    public static MetaObject forObject(Object object, ObjectFactory objectFactory, ObjectWrapperFactory objectWrapperFactory, ReflectorFactory reflectorFactory) {
-//        return null;
-//    }
-//    public abstract String findProperty(String propName, boolean useCamelCaseMapping);
-//    public abstract Object getOriginalObject();
-//    public abstract <E> void addAll(List<E> list);
-//    public abstract void add(Object element);
-//}
+package org.wing4j.litebatis.reflection;
+
+import java.util.List;
+
+/**
+ * Created by wing4j on 2017/5/16.
+ * 用于对实体类进行包装，提供字段、方法的元信息
+ */
+public interface MetaObject {
+    /**
+     * 返回是否有Getter方法
+     * @param fieldName 字段名
+     * @return 如果存在返回真
+     */
+    boolean hasGetter(String fieldName);
+    /**
+     * 返回是否有Setter方法
+     * @param fieldName 字段名
+     * @return 如果存在返回真
+     */
+    boolean hasSetter(String fieldName);
+
+    /**
+     * 设置字段值
+     * @param fieldName 字段名
+     * @param value 字段值
+     */
+    void setValue(String fieldName, Object value);
+
+    /**
+     * 获取字段值
+     * @param fieldName 字段名
+     * @param <T> 字段值
+     * @return 字段值
+     */
+    <T> T getValue(String fieldName);
+
+    /**
+     * 返回Setter方法的类型
+     * @param fieldName 字段名
+     * @return Setter方法的类型
+     */
+    Class<?> getSetterType(String fieldName);
+
+    /**
+     * 返回Getter方法的类型
+     * @param fieldName 字段名
+     * @return Getter方法的类型
+     */
+    Class<?> getGetterType(String fieldName);
+
+    /**
+     *
+     * @param propName
+     * @param useCamelCaseMapping
+     * @return
+     */
+    String findProperty(String propName, boolean useCamelCaseMapping);
+
+    Object getOriginalObject();
+
+    <E> void addAll(List<E> list);
+
+    void add(Object element);
+}
