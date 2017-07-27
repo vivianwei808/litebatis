4fded54fa76fc538a1c697c549e98509e9650b08
wing4j <wing4j@foxmail.com>
2017-07-27 22:01:25 +0800

    xiugai

--- litebatis-reflection/src/test/java/org/wing4j/litebatis/reflection/DefaultTypeAliasRegistryTest.java
+++ litebatis-reflection/src/test/java/org/wing4j/litebatis/reflection/DefaultTypeAliasRegistryTest.java
@@ -18,7 +18,26 @@ public class DefaultTypeAliasRegistryTest {
     @Test
     public void testResolveAlias() throws Exception {
         TypeAliasRegistry typeAliasRegistry = TypeAliasRegistryFactory.getInstance();
-        Class clazz = typeAliasRegistry.resolveAlias("int");
-        Assert.assertEquals(Integer.class, clazz);
+        Assert.assertEquals(Integer.class, typeAliasRegistry.resolveAlias("int"));
+        Assert.assertEquals(Integer.class, typeAliasRegistry.resolveAlias("integer"));
+        Assert.assertEquals(Integer.TYPE, typeAliasRegistry.resolveAlias("_int"));
+
+        Assert.assertEquals(Byte.class, typeAliasRegistry.resolveAlias("byte"));
+        Assert.assertEquals(Byte.TYPE, typeAliasRegistry.resolveAlias("_byte"));
+
+        Assert.assertEquals(Long.class, typeAliasRegistry.resolveAlias("long"));
+        Assert.assertEquals(Long.TYPE, typeAliasRegistry.resolveAlias("_long"));
+
+        Assert.assertEquals(Short.class, typeAliasRegistry.resolveAlias("short"));
+        Assert.assertEquals(Short.TYPE, typeAliasRegistry.resolveAlias("_short"));
+
+        Assert.assertEquals(Double.class, typeAliasRegistry.resolveAlias("double"));
+        Assert.assertEquals(Double.TYPE, typeAliasRegistry.resolveAlias("_double"));
+
+        Assert.assertEquals(Float.class, typeAliasRegistry.resolveAlias("float"));
+        Assert.assertEquals(Float.TYPE, typeAliasRegistry.resolveAlias("_float"));
+
+        Assert.assertEquals(Boolean.class, typeAliasRegistry.resolveAlias("boolean"));
+        Assert.assertEquals(Boolean.TYPE, typeAliasRegistry.resolveAlias("_boolean"));
     }
 }
